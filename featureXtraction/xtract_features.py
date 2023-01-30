80e0004c12f04c9bbc82bc21e7aa6e34fb22579d
genet <genetabay.s@gmail.comd>
2023-01-30 16:51:32 +0100

    feature extraction modified notes and more features added

--- featureXtraction/xtract_features.py
+++ featureXtraction/xtract_features.py
@@ -1,4 +1,16 @@
 import cv2
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+
+
+'''Notes
+The SIFT family of interest point detectors. Includes things like SURF, FREAK, BRISK, etc. Primarily used for object recognition, tracking and registration.
+HoG and it's variants. Used for object detection, handwritten digit recognition.
+LBP and variants. Previously used for face recognition and texture recognition applications.
+GIST and variants. Introduced for scene recognition and analysis. Similar in spirit to HoG and SIFT.
+How could we miss out the big daddy of features these days? Features learned on CNN for object recognition and detection.
+'''
 
 def get_sift_descriptor(gray_img):
     sift = cv2.SIFT_create()
@@ -6,15 +18,24 @@ def get_sift_descriptor(gray_img):
     return descriptors
 
 
-def get_pca_components(gray_img):
-    pass
+def get_surf_features(gray_img):
+    pca = PCA(n_components=30)
+    img_comp = pca.fit_transform(gray_img)
+    return img_comp
 
 
-def get_wavelet_components(gray_img):
+def get_hog_features(gray_img):
     pass
 
-def get_lda_components(gray_img):
-    pass
+def get_brisk_features(gray_img):
+    lda = LinearDiscriminantAnalysis()
+    lda.fit(X,y)
+    x_new = lda.transform(X) 
+
+def get_freak_features(gray_img):
+    lda = LinearDiscriminantAnalysis()
+    lda.fit(X,y)
+    x_new = lda.transform(X) 
 
 
 def get_features(list_img_gr, algorithm='sift'):
